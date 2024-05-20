b16e006f2457fdb3a1d57f45e54aa9322960bbc4
Eko Simanjuntak <lpt913@LPT-913.local>
2024-05-20 08:34:03 +0700

    chore: update kubernetes version and set node to 2
    Signed-off-by: Eko Simanjuntak <lpt913@LPT-913.local>

--- terraform/digital-ocean/kubernetes/playground/main.tf
+++ terraform/digital-ocean/kubernetes/playground/main.tf
@@ -2,11 +2,11 @@ resource "digitalocean_kubernetes_cluster" "this" {
   name   = "playground"
   region = "sgp1"
 
-  version = "1.28.2-do.0"
+  version = "1.29.1-do.0"
 
   node_pool {
     name       = "worker-pool-01"
     size       = "s-2vcpu-2gb"
-    node_count = 3
+    node_count = 2
   }
 }
